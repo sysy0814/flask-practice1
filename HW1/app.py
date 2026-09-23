5ad4ae93ff5a7cd6c5b4fbb6451251998a61dd85
이상윤 <sysy0814@gmail.com>
2026-09-23 18:05:28 +0900

    feat: 인사 페이지 추가

--- HW1/app.py
+++ HW1/app.py
@@ -22,5 +22,13 @@ def profile():
         hobbies=hobbies
     )
 
+# 인사 페이지
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template(
+        "greet.html",
+        name=name
+    )
+
 if __name__ == "__main__":
     app.run(debug=True)
